58f0ecfbfbd8e39d96cda71a1fe9d9fa6c257a23
CoderGirl <angi.adema@gmail.com>
2024-05-31 14:03:00 -0600

    added content to MapsPractice

--- src/com/examplefile/helloworld/MapsPractice.java
+++ src/com/examplefile/helloworld/MapsPractice.java
@@ -1,4 +1,8 @@
 package com.examplefile.helloworld;
 
 public class MapsPractice {
+    // Map uses key/value pairs inside a HashMap, for example.
+    public static void main(String[] args) {
+
+    }
 }
